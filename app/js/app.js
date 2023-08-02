a9d227a13ea15bc9b861ad5e7b748ea34fcdd519
Tomas <krainievlc@gmail.com>
2023-08-02 22:31:19 +0300

    Added works

--- app/js/app.js
+++ app/js/app.js
@@ -1,32 +1,58 @@
-import $ from 'jquery'; 
-window.jQuery = $; 
+import $ from 'jquery';
+import {getLinkPreview} from "link-preview-js";
+
+window.jQuery = $;
 window.$ = $; // import module example (npm i -D jquery)
 
-$(document).on('scroll', function() {
+const works = ['https://funtomas11.github.io/dental-meeting/'];
 
-	const header = document.querySelector('header');
-	header.classList.toggle('sticky', window.scrollY > 0);
+$(document).on('scroll', function () {
 
-	let sections = document.querySelectorAll('section');
-	let buttons = document.querySelectorAll('header > ul > li > a');
-	let pos = $(this).scrollTop() + $(this).scrollTop()*0.15;
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
 
-	
+    const sections = document.querySelectorAll('section');
+    const buttons = document.querySelectorAll('header > ul > li > a');
+    const pos = $(this).scrollTop() + $(this).scrollTop() * 0.15;
 
-	for(let i = 1; i < sections.length; i++)
-	{	
-		if ( pos >= $(sections[i]).position().top)				
-			buttons[i].classList.add('button_active');
-		
-		if ( $(this).scrollTop() >= $(sections[i]).position().top + $(sections[i]).height() || pos < $(sections[i]).position().top )
-			buttons[i].classList.remove('button_active');
-	}
-	
+    sections.forEach((section, index) => {
+        if (index === 0) {
+            return;
+        }
+
+        if (pos >= $(section).position().top) {
+            buttons[index].classList.add('button_active');
+        }
+
+        if ($(this).scrollTop() >= $(section).position().top + $(section).height() || pos < $(section).position().top) {
+            buttons[index].classList.remove('button_active');
+        }
+    });
+
 });
 
 window.toggleMenu = () => {
-	const menuToggle = document.querySelector('.toggle');
-	const menu = document.querySelector('.menu');
-	menuToggle.classList.toggle('active');
-	menu.classList.toggle('active');
+    const menuToggle = document.querySelector('.toggle');
+    const menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
 }
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const previews = [];
+    for (const work of works) {
+        previews.push(await getLinkPreview(work))
+    }
+
+    $('#work > .content').prepend(preparePreviewElement(previews[0]))
+})
+
+function preparePreviewElement(preview) {
+    const newElement = document.createElement('div');
+    newElement.className = 'work__box';
+    const imageElement = document.createElement('img');
+    imageElement.setAttribute('src', preview.images[0])
+    newElement.appendChild(imageElement)
+    return newElement;
+}
+
